6c55eea895f6bbda7b1eaae8af92511afc3dff9e
Lucas Azael Linhares Soares <azaelsoares@gmail.com>
2020-05-27 01:54:17 -0300

    db api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'tasklist',
+    loadChildren: () => import('./pages/tasklist/tasklist.module').then( m => m.TasklistPageModule)
+  },
 
 ];
 
